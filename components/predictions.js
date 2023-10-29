66c47b430a1cb277a7511c33befd21c2e2e7e71f
Akash Sahu <aakashsahu1511@gmail.com>
2023-10-29 23:56:55 +0530

    style: added  ui improvements

--- components/predictions.js
+++ components/predictions.js
@@ -9,11 +9,11 @@ export const Predictions = ({ newsUrl, newsCategory }) => {
     //   </div>
     // </div>
 
-    <div className="flex items-center justify-between">
-      <div className="flex flex-col items-left justify-center py-4 px-10 w-13">
+    <div className="max-w-screen overflow-hidden rounded-xl bg-white m-4 p-4">
+      <div className="flex  justify-center py-4 mx-8  text-justify">
         {newsUrl}
       </div>
-      <div className="flex flex-col items-center justify-center py-4 px-10">
+      <div className="flex items-center justify-center py-1 px-10">
         {newsCategory}
       </div>
     </div>
